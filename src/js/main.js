151cfe76adf20532e874ede01438771bc5e4f27e
unknown <vladimirovicp70@gmail.com>
2026-01-22 22:23:50 +0400

    feat: add go-top

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,23 @@
-console.log('vpn time');
+const goTopBtn = document.querySelector(".go-top");
+
+if(goTopBtn){
+    
+    goTopBtn.addEventListener("click", goTop);
+    window.addEventListener("scroll", trackScroll);
+    function trackScroll() {
+        const scrolled = window.pageYOffset;
+
+        if (scrolled > 200) {
+            goTopBtn.classList.add("go-top--show");
+        } else {
+            goTopBtn.classList.remove("go-top--show");
+        }
+    }
+
+    function goTop() {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    }
+}
